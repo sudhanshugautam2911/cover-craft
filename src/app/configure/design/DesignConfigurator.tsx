d10ad642793b14420ffcd9b4476ee4d6e6fbadda
sudhanshugautam2911 <gautamsudhanshu2911@gmail.com>
2024-08-01 17:05:43 +0530

    Working on cover design configurator

---
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -0,0 +1,31 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio";
+import NextImage from 'next/image'
+
+// Learning - whole component is just amazing
+interface DesignConfiguratorProps {
+    configId: string
+    imageUrl: string
+    imageDimensions: { width: number; height: number }
+}
+
+const DesignConfigurator = ({ configId, imageUrl, imageDimensions }: DesignConfiguratorProps) => {
+    return (
+        <div className='relative mt-20 grid grid-cols-1 lg:grid-cols-3 mb-20 pb-20'>
+            <div
+                // ref={containerRef}
+                className='relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2'>
+                <div className='relative w-60 bg-opacity-50 pointer-events-none aspect-[896/1831]'>
+                    {/* shadcn component to always maintain same aspect ratio */}
+                    <AspectRatio ratio={896 / 1831} className="pointer-events-none relative z-50 aspect-[896/1831] w-full">
+                        <NextImage fill alt='phone image' src='/phone-template.png' className="pointer-events-none z-50 select-none" />
+                    </AspectRatio>
+                    {/* shadow to mobile phone */}
+                    <div className='absolute z-40 inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] shadow-[0_0_0_99999px_rgba(229,231,235,0.6)]' />
+                    
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DesignConfigurator
--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,8 +1,37 @@
+import { db } from "@/db";
+import { notFound } from "next/navigation";
+import DesignConfigurator from "./DesignConfigurator";
 
-const Page = async () => {
-    // make db call
+interface PageProps {
+  searchParams: {
+    [key: string]: string | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  // make db call
+  const { id } = searchParams;
+
+  if (!id || typeof id !== 'string') {
+    return notFound();
+  }
+
+  const configuration = await db.configuration.findUnique({
+    where: { id }
+  })
+
+  if (!configuration) {
+    return notFound();
+  }
+
+  const { imageUrl, width, height } = configuration;
+
   return (
-    <div>Page</div>
+    // resizing can be done in client side component only so creating a client side compo.
+    <DesignConfigurator
+      imageDimensions={{ width, height }}
+      configId={configuration.id}
+      imageUrl={configuration.imageUrl} />
   )
 }
 
