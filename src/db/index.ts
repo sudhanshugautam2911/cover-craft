b9faee077b21becf8d2c469961f9cfd8e9ec220d
sudhanshugautam2911 <gautamsudhanshu2911@gmail.com>
2024-08-27 20:52:57 +0530

    test

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,12 +1,21 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '@prisma/client'
 
-let prisma = new PrismaClient();
 
-// Optional: Add error handling for better debugging
-try {
-  prisma.$connect();
-} catch (error) {
-  console.error('Failed to connect to the database:', error);
+// learning - this file is only to avoid creating prisma client again and again in each file but still it is not necessary
+
+declare global {
+  var cachedPrisma: PrismaClient
 }
 
-export const db = prisma;
+let prisma: PrismaClient
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.cachedPrisma) {
+    global.cachedPrisma = new PrismaClient()
+  }
+
+  prisma = global.cachedPrisma
+}
+
+export const db = prisma
